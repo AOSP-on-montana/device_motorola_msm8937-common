60c52d9ccccf09edea666cf4d16272e1619cdced
Nolen Johnson <johnsonnolen@gmail.com>
2021-01-03 12:58:57 -0500

    msm8937-common: Use hardware/motorola's common MotoActions
    Change-Id: Ic730e45b25c87fb5b9d8d301bf44bd7176a0576c

--- msm8937.mk
+++ msm8937.mk
@@ -206,12 +206,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.msm8937
 
-# LineageActions
-ifeq ($(filter ahannah rhannah,$(TARGET_DEVICE)),)
-PRODUCT_PACKAGES += \
-    LineageActions
-endif
-
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
@@ -220,6 +214,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+
+# MotoActions
+ifeq ($(filter ahannah rhannah,$(TARGET_DEVICE)),)
+PRODUCT_PACKAGES += \
+    MotoActions
+endif
 
 # OMX
 PRODUCT_PACKAGES += \
