b355f6872bc0ba7757fd44da1080834efdcfbd5c
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-08 09:48:37 -0400

    msm8937-common: Ship copy of API 28 cgroups/task_profiles.json in vendor
    Google's prebuilt cgroups.json and task_profiles.json for products
    launched with previous API levels only covers
    ro.product.first_api_level >= 28. [1]
    
    Motorola msm8937 devices were launched with Nougat/Nougat MR1/Oreo (API levels 24-26), so schedtune and
    task groups are completely broken. Since the system also checks
    /vendor/etc for vendor profiles, make a copy of cgroups_28.json
    and task_profiles_28.json and ship them to /vendor/etc. Profiles
    for previous API levels are all the same anyway.
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- msm8937.mk
+++ msm8937.mk
@@ -80,6 +80,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.5-service
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # CNE
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
